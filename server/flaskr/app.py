3048077a29cae70bfecdf8835c5d6b243c6ac708
Jamie Ingram <jr-ingram@hotmail.co.uk>
2024-04-06 14:49:51 +0100

    Fix majority of app.py linting issues, minus long import

--- server/flaskr/app.py
+++ server/flaskr/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, url_for, jsonify, request
+"""
+    Flask server for dnd5e-dungeon-planner.
+    Primarily interacts with the postgresql database and serves records to the users.
+"""
+
+from flask import Flask, jsonify, request
 from db import select_all_dungeons, select_dungeon_by_id, select_all_traps, select_trap_by_id, delete_trap, insert_trap, update_trap
 
 app = Flask(__name__)
@@ -6,12 +11,14 @@ app = Flask(__name__)
 
 @app.route("/dungeon")
 def get_dungeon():
+    """Fetches all dungeons in the database"""
     dungeons = select_all_dungeons()
     return jsonify(dungeons)
 
 
 @app.route("/dungeon/<int:dungeon_id>")
 def get_dungeon_by_id(dungeon_id):
+    """Fetches the dungeon that matches dungeon_id from the database"""
     dungeon = select_dungeon_by_id(dungeon_id)
     if dungeon is None:
         return jsonify({})
@@ -20,18 +27,21 @@ def get_dungeon_by_id(dungeon_id):
 
 @app.route('/trap')
 def get_trap():
+    """Fetches all traps in the database"""
     traps = select_all_traps()
     return jsonify(traps)
 
 
 @app.route('/trap/<int:trap_id>')
 def get_trap_by_id(trap_id):
+    """Fetches the trap that matches the trap_id from the database"""
     trap = select_trap_by_id(trap_id)
     return jsonify(trap)
 
 
 @app.route('/trap', methods=['POST'])
 def create_trap():
+    """Creates a trap using trap_name and trap_effect"""
     trap_name = request.form['trap_name']
     trap_effect = request.form['trap_effect']
     trap = insert_trap(trap_name, trap_effect)
@@ -40,6 +50,7 @@ def create_trap():
 
 @app.route('/trap/<int:trap_id>', methods=['PUT'])
 def edit_trap(trap_id):
+    """Updates the trap in the database that matches trap_id"""
     trap_name = request.form['trap_name']
     trap_effect = request.form['trap_effect']
     edited_trap = update_trap(trap_id, trap_name, trap_effect)
@@ -48,10 +59,6 @@ def edit_trap(trap_id):
 
 @app.route('/trap/<int:trap_id>', methods=['DELETE'])
 def delete_trap_by_id(trap_id):
+    """Deletes trap from the database that matches trap_id"""
     trap = delete_trap(trap_id)
     return jsonify(trap)
-
-
-with app.test_request_context():
-    print(url_for('get_dungeon'))
-    print(url_for('get_dungeon_by_id', dungeon_id='10'))
