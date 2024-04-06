3554b40a929051faafb3625e14ad9aeb21e90450
Jamie Ingram <jr-ingram@hotmail.co.uk>
2024-04-06 13:47:02 +0100

    feat: add trap endpoints

--- server/flaskr/app.py
+++ server/flaskr/app.py
@@ -1,23 +1,48 @@
-from flask import Flask, url_for
-from db import select_all_dungeons, select_dungeon_by_id
+from flask import Flask, url_for, jsonify, request
+from db import select_all_dungeons, select_dungeon_by_id, select_all_traps, select_trap_by_id, delete_trap, insert_trap, update_trap
 
 app = Flask(__name__)
 
 @app.route("/dungeon")
 def get_dungeon():
     dungeons = select_all_dungeons()
-    dungeon_string = ""
-    for dungeon in dungeons:
-        dungeon_string = f'{dungeon_string}<li>{dungeon}</li>'
-
-    return f"<h1>Dungeons</h1><ul>{dungeon_string}</ul>"
+    return jsonify(dungeons)
 
 @app.route("/dungeon/<int:dungeon_id>")
 def get_dungeon_by_id(dungeon_id):
     dungeon = select_dungeon_by_id(dungeon_id)
     if dungeon is None:
-        return f'<p>a dungeon with id {dungeon_id} does not exist</p>'
-    return f'<p>{dungeon}</p>'
+        return jsonify({})
+    return jsonify(dungeon)
+
+@app.route('/trap')
+def get_trap():
+    traps = select_all_traps()
+    return jsonify(traps)
+
+@app.route('/trap/<int:trap_id>')
+def get_trap_by_id(trap_id):
+    trap = select_trap_by_id(trap_id)
+    return jsonify(trap)
+
+@app.route('/trap', methods=['POST'])
+def create_trap():
+    trap_name = request.form['trap_name']
+    trap_effect = request.form['trap_effect']
+    trap = insert_trap(trap_name, trap_effect)
+    return jsonify(trap)
+
+@app.route('/trap/<int:trap_id>', methods=['PUT'])
+def edit_trap(trap_id):
+    trap_name = request.form['trap_name']
+    trap_effect = request.form['trap_effect']
+    edited_trap = update_trap(trap_id, trap_name, trap_effect)
+    return jsonify(edited_trap)
+
+@app.route('/trap/<int:trap_id>', methods=['DELETE'])
+def delete_trap_by_id(trap_id):
+    trap = delete_trap(trap_id)
+    return jsonify(trap)
 
 with app.test_request_context():
     print(url_for('get_dungeon'))
--- server/flaskr/db.py
+++ server/flaskr/db.py
@@ -12,6 +12,7 @@ def connect_to_db():
         password=os.getenv('DB_PASSWORD'),
         port=os.getenv('DB_PORT')
     )
+    connection.autocommit = True
     cursor = connection.cursor()
     return cursor
 
@@ -29,3 +30,40 @@ def select_dungeon_by_id(dungeon_id: str):
     cursor.close()
     return dungeon
 
+def select_all_traps():
+    cursor = connect_to_db()
+    cursor.execute("SELECT * FROM trap")
+    traps = cursor.fetchall()
+    cursor.close()
+    return traps
+
+def select_trap_by_id(trap_id: str):
+    cursor = connect_to_db()
+    cursor.execute("SELECT * FROM trap WHERE id = %s", (trap_id, ))
+    trap = cursor.fetchone()
+    cursor.close()
+    return trap
+
+def insert_trap(trap_name: str, trap_effect: str):
+    cursor = connect_to_db()
+    sql_query = "INSERT INTO trap(name, effect) VALUES(%s, %s) RETURNING *"
+    cursor.execute(sql_query, (trap_name, trap_effect, ))
+    trap = cursor.fetchone()
+    cursor.close()
+    return trap
+
+def update_trap(trap_id: str, trap_name: str, trap_effect: str):
+    cursor = connect_to_db()
+    sql_query = "UPDATE trap SET name=%s, effect=%s WHERE id = %s RETURNING *"
+    cursor.execute(sql_query, (trap_name, trap_effect, trap_id, ))
+    trap = cursor.fetchone()
+    cursor.close()
+    return trap
+
+def delete_trap(trap_id: str):
+    cursor = connect_to_db()
+    cursor.execute('DELETE FROM room_trap WHERE trap_id = %s RETURNING *;', (trap_id, ))
+    cursor.execute('DELETE FROM trap WHERE id = %s RETURNING *;', (trap_id, ))
+    deleted_trap = cursor.fetchone()
+    cursor.close()
+    return deleted_trap
