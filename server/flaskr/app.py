ef844b2481bf614799a83e8de52350e313507703
J. Ingram <JRIngram@users.noreply.github.com>
2024-04-06 16:53:38 +0100

    CI: Add server workflow, linting and DB gateway classes (#2)
    * Add server workflow and linting
    
    * Fix linting issues for db.py and fix formatting across server .py files
    
    * Fix majority of app.py linting issues, minus long import
    
    * Add DB gateway singleton classes
    
    * Add pylintrc and update workflow

--- server/flaskr/app.py
+++ server/flaskr/app.py
@@ -1,49 +1,68 @@
-from flask import Flask, url_for, jsonify, request
-from db import select_all_dungeons, select_dungeon_by_id, select_all_traps, select_trap_by_id, delete_trap, insert_trap, update_trap
+"""
+    Flask server for dnd5e-dungeon-planner.
+    Primarily interacts with the postgresql database and serves records to the users.
+"""
+
+from flask import Flask, jsonify, request
+from db.dungeon import DungeonGatewaySingleton
+from db.trap import TrapGatewaySingleton
 
 app = Flask(__name__)
 
+trap_gateway = TrapGatewaySingleton()
+dungeon_gateway = DungeonGatewaySingleton()
+
+
 @app.route("/dungeon")
 def get_dungeon():
-    dungeons = select_all_dungeons()
+    """Fetches all dungeons in the database"""
+    dungeons = dungeon_gateway.select_all_dungeons()
     return jsonify(dungeons)
+
 
 @app.route("/dungeon/<int:dungeon_id>")
 def get_dungeon_by_id(dungeon_id):
-    dungeon = select_dungeon_by_id(dungeon_id)
+    """Fetches the dungeon that matches dungeon_id from the database"""
+    dungeon = dungeon_gateway.select_dungeon_by_id(dungeon_id)
     if dungeon is None:
         return jsonify({})
     return jsonify(dungeon)
 
+
 @app.route('/trap')
 def get_trap():
-    traps = select_all_traps()
+    """Fetches all traps in the database"""
+    traps = trap_gateway.select_all_traps()
     return jsonify(traps)
+
 
 @app.route('/trap/<int:trap_id>')
 def get_trap_by_id(trap_id):
-    trap = select_trap_by_id(trap_id)
+    """Fetches the trap that matches the trap_id from the database"""
+    trap = trap_gateway.select_trap_by_id(trap_id)
     return jsonify(trap)
+
 
 @app.route('/trap', methods=['POST'])
 def create_trap():
+    """Creates a trap using trap_name and trap_effect"""
     trap_name = request.form['trap_name']
     trap_effect = request.form['trap_effect']
-    trap = insert_trap(trap_name, trap_effect)
+    trap = trap_gateway.insert_trap(trap_name, trap_effect)
     return jsonify(trap)
+
 
 @app.route('/trap/<int:trap_id>', methods=['PUT'])
 def edit_trap(trap_id):
+    """Updates the trap in the database that matches trap_id"""
     trap_name = request.form['trap_name']
     trap_effect = request.form['trap_effect']
-    edited_trap = update_trap(trap_id, trap_name, trap_effect)
+    edited_trap = trap_gateway.update_trap(trap_id, trap_name, trap_effect)
     return jsonify(edited_trap)
+
 
 @app.route('/trap/<int:trap_id>', methods=['DELETE'])
 def delete_trap_by_id(trap_id):
-    trap = delete_trap(trap_id)
+    """Deletes trap from the database that matches trap_id"""
+    trap = trap_gateway.delete_trap(trap_id)
     return jsonify(trap)
-
-with app.test_request_context():
-    print(url_for('get_dungeon'))
-    print(url_for('get_dungeon_by_id', dungeon_id='10'))
---
+++ server/flaskr/db/trap.py
@@ -0,0 +1,57 @@
+"""
+    A gateway file for interacting with the trap table within the postgreSQL database
+"""
+from db.singleton_metaclass import SingletonMeta
+from db.connect_to_db import connect_to_db
+
+
+class TrapGatewaySingleton(metaclass=SingletonMeta):
+    """
+        A gateway class for the trap table in the postgreSQL database
+    """
+    def select_all_traps(self):
+        """Returns the results of a `SELECT * trap` from the DB"""
+        cursor = connect_to_db()
+        cursor.execute("SELECT * FROM trap")
+        traps = cursor.fetchall()
+        cursor.close()
+        return traps
+
+    def select_trap_by_id(self, trap_id: str):
+        """
+            Returns the results of a `SELECT * FROM trap WHERE id = trap_id` from the DB
+        """
+        cursor = connect_to_db()
+        cursor.execute("SELECT * FROM trap WHERE id = %s", (trap_id, ))
+        trap = cursor.fetchone()
+        cursor.close()
+        return trap
+
+    def insert_trap(self, trap_name: str, trap_effect: str):
+        """Inserts a new trap record into the DB"""
+        cursor = connect_to_db()
+        sql_query = "INSERT INTO trap(name, effect) VALUES(%s, %s) RETURNING *"
+        cursor.execute(sql_query, (trap_name, trap_effect, ))
+        trap = cursor.fetchone()
+        cursor.close()
+        return trap
+
+    def update_trap(self, trap_id: str, trap_name: str, trap_effect: str):
+        """Updates the trap record in the db that matches trap_id"""
+        cursor = connect_to_db()
+        sql_query = "UPDATE trap SET name=%s, effect=%s WHERE id = %s RETURNING *"
+        cursor.execute(sql_query, (trap_name, trap_effect, trap_id, ))
+        trap = cursor.fetchone()
+        cursor.close()
+        return trap
+
+    def delete_trap(self, trap_id: str):
+        """Deletes the trap record in the db that matches the trap_id"""
+        cursor = connect_to_db()
+        cursor.execute(
+            'DELETE FROM room_trap WHERE trap_id = %s RETURNING *;', (trap_id, ))
+        cursor.execute(
+            'DELETE FROM trap WHERE id = %s RETURNING *;', (trap_id, ))
+        deleted_trap = cursor.fetchone()
+        cursor.close()
+        return deleted_trap
