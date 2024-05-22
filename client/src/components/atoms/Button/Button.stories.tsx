1da8ebfcf6203c392c995ecf6be75ad579d28f33
Jamie Ingram <jr-ingram@hotmail.co.uk>
2024-05-22 18:43:06 +0100

    Add button variants

--- client/src/components/atoms/Button/Button.stories.tsx
+++ client/src/components/atoms/Button/Button.stories.tsx
@@ -16,5 +16,6 @@ export const Primary: Story = {
     text: "Press me!",
     ariaLabel: "Press me!",
     onClick: () => console.log("clicked"),
+    variant: "primaryFilled"
   },
 };
--- client/src/components/atoms/Button/Button.tsx
+++ client/src/components/atoms/Button/Button.tsx
@@ -1,12 +1,39 @@
+type Variants = "primaryFilled" | "primaryOutline";
+
 type Props = {
   text: string;
-  ariaLabel: string;
   onClick: () => void;
+  ariaLabel?: string;
+  variant: Variants;
 };
 
-export const Button = ({ text, ariaLabel, onClick }: Props) => {
+const styles = {
+  base: "border-4 rounded-full p-3",
+  variants: {
+    primary: {
+      base: `border-primary-200 hover:bg-primary-50 hover:text-primary-200 active:bg-primary-50 active:bg-primary-50 active:border-primary-50`,
+      outline: `text-primary-200`,
+      filled: `text-white bg-primary-200`,
+    },
+  },
+};
+
+export const Button = ({ text, ariaLabel, onClick, variant }: Props) => {
+  const getVariantStyle = () => {
+    switch (variant) {
+      case "primaryFilled":
+        return `${styles.base} ${styles.variants.primary.base} ${styles.variants.primary.filled}`;
+      case "primaryOutline":
+        return `${styles.base} ${styles.variants.primary.base} ${styles.variants.primary.outline}`;
+    }
+  };
+
   return (
-    <button aria-label={ariaLabel} onClick={onClick}>
+    <button
+      aria-label={ariaLabel}
+      onClick={onClick}
+      className={getVariantStyle()}
+    >
       {text}
     </button>
   );
