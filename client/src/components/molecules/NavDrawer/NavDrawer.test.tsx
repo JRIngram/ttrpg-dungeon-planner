8492ab5aaf1f3e0917a31955623adc7bf69e4083
Jamie Ingram <jr-ingram@hotmail.co.uk>
2024-07-30 21:01:39 +0100

    Add default option props to NavDrawer

--- client/src/components/molecules/NavDrawer/NavDrawer.test.tsx
+++ client/src/components/molecules/NavDrawer/NavDrawer.test.tsx
@@ -1,15 +1,18 @@
 import { vi, describe, it, expect, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { composeStory } from "@storybook/react";
 
-import Meta, { Default } from "./NavDrawer.stories";
 import userEvent from "@testing-library/user-event";
-import { DrawerItem } from "./NavDrawer";
+import { DrawerItem, NavDrawer } from "./NavDrawer";
 
 const onSelectSpy = vi.fn();
+const onDefaultSelectedSpy = vi.fn();
 
-const renderButtonRow = () => {
-  const NavDrawer = composeStory(Default, Meta);
+const renderButtonRow = (withDefault = false) => {
+  const defaultItem = {
+    label: "Default Item",
+    onDefaultSelected: onDefaultSelectedSpy,
+  };
+
   const items: DrawerItem[] = [
     {
       label: "Item One",
@@ -24,6 +27,17 @@ const renderButtonRow = () => {
       id: "3",
     },
   ];
+
+  if (withDefault) {
+    return render(
+      <NavDrawer
+        items={items}
+        onSelect={(id) => onSelectSpy(id)}
+        defaultItem={defaultItem}
+      />
+    );
+  }
+
   return render(<NavDrawer items={items} onSelect={(id) => onSelectSpy(id)} />);
 };
 
@@ -36,6 +50,7 @@ describe("ButtonRow", () => {
     renderButtonRow();
     expect(screen.getByRole("button", { name: "Item One" })).toBeVisible();
     expect(screen.getByRole("button", { name: "Item Two" })).toBeVisible();
+    expect(screen.getByRole("button", { name: "Item Three" })).toBeVisible();
   });
 
   it("calls respective onClick functions when each button is pressed", async () => {
@@ -48,5 +63,35 @@ describe("ButtonRow", () => {
     await userEvent.click(screen.getByRole("button", { name: "Item Three" }));
     expect(onSelectSpy).toHaveBeenCalledWith("3");
     expect(onSelectSpy).toHaveBeenCalledTimes(3);
+  });
+
+  describe("Default option", () => {
+    it("renders all buttons passed and default option", () => {
+      renderButtonRow(true);
+      expect(
+        screen.getByRole("button", { name: "Default Item" })
+      ).toBeVisible();
+      expect(screen.getByRole("button", { name: "Item One" })).toBeVisible();
+      expect(screen.getByRole("button", { name: "Item Two" })).toBeVisible();
+      expect(screen.getByRole("button", { name: "Item Three" })).toBeVisible();
+    });
+
+    it("calls respective onClick functions when each button is pressed", async () => {
+      renderButtonRow(true);
+      expect(onDefaultSelectedSpy).toBeCalledTimes(0);
+      await userEvent.click(screen.getByRole("button", { name: "Item One" }));
+      expect(onSelectSpy).toHaveBeenCalledWith("1");
+      await userEvent.click(screen.getByRole("button", { name: "Item Two" }));
+      expect(onSelectSpy).toHaveBeenCalledWith("2");
+      await userEvent.click(screen.getByRole("button", { name: "Item Three" }));
+      expect(onSelectSpy).toHaveBeenCalledWith("3");
+      expect(onSelectSpy).toHaveBeenCalledTimes(3);
+
+      expect(onDefaultSelectedSpy).toBeCalledTimes(0);
+      await userEvent.click(
+        screen.getByRole("button", { name: "Default Item" })
+      );
+      expect(onDefaultSelectedSpy).toHaveBeenCalledOnce();
+    });
   });
 });
