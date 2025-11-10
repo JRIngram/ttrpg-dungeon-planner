07b6acd07e91182c19ce0cfa6a487107af3b0fa2
Jamie Ingram <jr-ingram@hotmail.co.uk>
2025-11-10 18:56:01 +0000

    Improve handling of quantity selector initial value

--- client/src/components/molecules/ItemQuantitySelector/ItemQuantitySelector.test.tsx
+++ client/src/components/molecules/ItemQuantitySelector/ItemQuantitySelector.test.tsx
@@ -11,10 +11,10 @@ describe("ItemQuantitySelector", () => {
     render(<ItemQuantitySelector />);
 
     expect(
-      screen.getByRole("textbox", { name: "Item quantity" }),
+      screen.getByRole("textbox", { name: "Item quantity" })
     ).toBeVisible();
     expect(
-      screen.getByRole("combobox", { name: "Select an Item" }),
+      screen.getByRole("combobox", { name: "Select an Item" })
     ).toBeVisible();
   });
 
@@ -40,7 +40,7 @@ describe("ItemQuantitySelector", () => {
           options: options,
           placeholder: "Select an item.",
         }}
-      />,
+      />
     );
 
     const quantitySelector = screen.getByRole("textbox", {
@@ -55,10 +55,55 @@ describe("ItemQuantitySelector", () => {
       itemSelector,
       screen.getByRole("option", {
         name: options[0].label,
-      }),
+      })
     );
 
     expect(quantitySelector).toHaveValue("123");
     expect(itemSelector).toHaveValue(options[0].value);
+  });
+
+  it("displays default value", async () => {
+    const ItemQuantitySelector = composeStory(Default, meta);
+    const options = [
+      {
+        label: "Item One",
+        value: "1",
+      },
+      {
+        label: "Item Two",
+        value: "2",
+      },
+      {
+        label: "Item Three",
+        value: "3",
+      },
+    ];
+    const initialValue = { itemValue: "3", quantity: "4" };
+
+    render(
+      <ItemQuantitySelector
+        dropdownConfig={{
+          options: options,
+          placeholder: "Select an item.",
+        }}
+        initialValue={initialValue}
+      />
+    );
+
+    const quantitySelector = screen.getByRole("textbox", {
+      name: "Item quantity",
+    });
+    const itemSelector = screen.getByRole("combobox", {
+      name: "Select an Item",
+    });
+
+    const initialItemIndex = options.findIndex(
+      (o) => o.value === initialValue.itemValue
+    );
+    const initialItemLabel = options[initialItemIndex].label;
+
+    expect(quantitySelector).toHaveValue(initialValue.quantity);
+    expect(itemSelector).toHaveValue(initialValue.itemValue);
+    expect(screen.getByText(initialItemLabel)).toBeVisible();
   });
 });
--- client/src/components/molecules/ItemQuantitySelector/ItemQuantitySelector.tsx
+++ client/src/components/molecules/ItemQuantitySelector/ItemQuantitySelector.tsx
@@ -1,15 +1,18 @@
 import { Dropdown, DropdownProps } from "@/components/atoms/Dropdown/Dropdown";
 import { TextInput } from "@/components/atoms/TextInput/TextInput";
 
+type ItemQuantityPair = {
+  itemValue: string;
+  quantity: string;
+};
+
 export type ItemQuantitySelectorProps = {
   id: string;
   itemName: string;
   textInputFormName: string;
-  dropdownConfig: Pick<
-    DropdownProps,
-    "placeholder" | "options" | "initialOption"
-  >; // just include placeholder and options
+  dropdownConfig: Pick<DropdownProps, "placeholder" | "options">; // just include placeholder and options
   isRequired?: boolean;
+  initialValue?: ItemQuantityPair;
 };
 
 export const ItemQuantitySelector = ({
@@ -18,6 +21,7 @@ export const ItemQuantitySelector = ({
   textInputFormName,
   dropdownConfig,
   isRequired,
+  initialValue,
 }: ItemQuantitySelectorProps) => {
   return (
     <div className="flex gap-4 justify-between" id={id}>
@@ -28,6 +32,9 @@ export const ItemQuantitySelector = ({
         placeholder="Enter a quantity"
         pattern="\d*"
         isRequired={isRequired}
+        initialValue={
+          initialValue !== undefined ? initialValue.quantity : undefined
+        }
       />
 
       <Dropdown
@@ -37,7 +44,13 @@ export const ItemQuantitySelector = ({
         placeholder={dropdownConfig.placeholder}
         isRequired={isRequired}
         options={dropdownConfig.options}
-        initialOption={dropdownConfig.initialOption}
+        initialOption={
+          initialValue !== undefined
+            ? dropdownConfig.options.find(
+                (o) => o.value === initialValue.itemValue
+              )
+            : undefined
+        }
       />
     </div>
   );
