cec4d6bf0765513205dfe1564331c1584165e178
Jamie Ingram <jr-ingram@hotmail.co.uk>
2026-03-28 17:51:54 +0000

    fix: add config page

--- client/src/app/configs/page.tsx
+++ client/src/app/configs/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 import { NavDrawer } from "@/components/molecules/NavDrawer/NavDrawer";
-import { EncounterMultiplierService } from "@/services/EncounterMultiplierService/EncounterMultiplierService";
-import { useQuery } from "@tanstack/react-query";
 
 export default function Configs() {
-  const encounterMultiplierService = new EncounterMultiplierService();
-
-  const { data, isLoading, isError, refetch } = useQuery({
-    queryKey: ["encounter-multiplier-service"],
-    queryFn: () => {
-      return encounterMultiplierService.getList();
-    },
-  });
-
-  console.log({ data });
-
   return (
     <div className="flex">
       <NavDrawer items={[]} onSelect={() => {}} />
